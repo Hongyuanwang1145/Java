bf43825665d000dfef6041247a4236c626adfa05
Hongyuanwang1145 <hongyuanw@nycstudents.net>
2026-01-05 16:52:03 +0000

    h

---
+++ Java-8-Activity/Main.java
@@ -0,0 +1,49 @@
+class Main {
+
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+  //1: printing a string
+  void great(String msg){
+    System.out.println(msg);
+  }
+    great("Hi");
+  //2: Fahrenheit to Celsius temperature conversion
+  double F to C = (double F){
+    double C =(F-32)*5/9;
+    return C;
+    System.out.println("finder atemp in dyF");
+    
+  }
+
+
+  //3: Sphere volume
+
+  
+  //4: Cone volume
+
+
+  //5: Distance between two coordinate points
+
+
+
+  
+  void init(){
+    //1
+    System.out.println("Enter your name ");
+    String name = Input.readString();
+    print("Welcome "+name);
+
+    //2
+
+    //3
+
+    //4
+
+    //5
+    
+    
+  }
+ 
+}
