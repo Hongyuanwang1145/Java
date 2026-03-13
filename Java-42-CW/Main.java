00db62ef2025d306d3eede9b9a4fd279f85e6e22
Hongyuanwang1145 <hongyuanw@nycstudents.net>
2026-03-13 16:31:12 +0000

    g

---
+++ Java-42-CW/Main.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.sql.*;
+// To Compile on the Replit shell use: (same as for mac)
+// javac -cp sqlite-jdbc-3.23.1.jar: Main.java
+
+// To compile in Windows use:
+// javac -cp sqlite-jdbc-3.23.1.jar
+
+// To execute(run) use:
+// java -cp sqlite-jdbc-3.23.1.jar: Main
+class Main {
+
+ public static void main(String[] args)throws IOException{
+    (new Main()).init();
+  }
+
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  void init() {
+     
+    String sql = "";
+    String queryResult = "";
+    // set up appropriate DB object to students.db
+    Database db = new Database("jdbc:sqlite:");	
+
+    // 1: Add the course “MCLUB1” section 3 during period 1, with teacher1 as “WHOKNOWS”, room is “ROOFTOP” for STUDENT2.
+    sql = "Insert Into section3  period1 ";
+    sql += " (teacher1, room, student2, section, period)";
+    sql += "Values ('WHOKNOWS', 'ROOFTOP' , 'STUDENT2', 'SECTION3 , PERIOD1')";
+  
+    db.runSQL(sql);
+    
+    // 2: For student with ID “STUDENT4”, add course “CN100” with a section 1, teacher1 as  “JOSEPH”, period 2 and room 108.
+    sql = "Insert studentid, course, teacher, period2, room108";
+    sql += " (student, CN100, JOSEPH, period2, 108)";
+    sql += "Values ('STUDENT4', 'CN100', 'JOSEPH',)";
+  
+    db.runSQL(sql);
+      
+  }    
+}
