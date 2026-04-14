95271ffa7271f48dff1a3e53837ac02ebe93a310
Hongyuanwang1145 <hongyuanw@nycstudents.net>
2026-04-14 16:25:14 +0000

    o

--- Java-48-CW/script.js
+++ Java-48-CW/script.js
@@ -2,7 +2,7 @@ let data, info; // global variables
 
 async function init(){  
   let link = "";// Insert your Dev URL from Activity 47. The activity (DB server) must be running in a separate tab.
-  let route= "";
+  let route= "/employees";
 
   /*
   For the problems below, use the DB server you created for Activity 47 and start it. If you did not complete the first 2 problems of activity 47, then use the database server link given in Lesson 48-Discussion solution (today's lesson).
@@ -14,7 +14,10 @@ async function init(){
 
   
   // Problem 2: Write the JS code in the function init() and get the employee's table from your server as an array of JSON.
+  info = await fetch(link+route);
+  info = await info.json;
 
+  let employees = data;
   
   // Problem 3: For each employee, display their first name, last name and birthdate to the console.
 
