fa05a2f53135516c8ec1e6cfbc8c66fc02743b74
Hongyuanwang1145 <hongyuanw@nycstudents.net>
2026-04-30 16:22:55 +0000

    ttt

--- Java-54-Activity/script.js
+++ Java-54-Activity/script.js
@@ -2,7 +2,49 @@ let customers, info;
 
 async function init(){
   
- 
+ info = await fetch(link+route);
+  customers = await info.json();
 
+  generateCards(customers);
 }
+
+function generateCards(customers){
+  let centerpanel = document.getElementById("centerpanel");
+  let build ="";
+
+  for(let i=0; i<customers.length; i++){
+    let customer = customers[i];
+    build += `<div class="card" >`
+    build += `<h3> Customer ID : ${customer.CustomerId}</h3>`;
+    build += `<div> First Name : ${customer.FirstName}</div>`;
+    build += `<div> Last Name : ${customer.LastName}</div>`;
+    build += `<div> Country : ${customer.Country}</div>`;
+    build += `<p> Email : ${customer.Email}</p>`;
+    build += `<img src='countries/${customer.Country}.PNG'>`;
+    build += `<hr>`;
+    build += `</div>`;
+  }
+  // Now inject the build content into the output container
+  centerpanel.innerHTML = build;
+}
+
+function filter(){
+  let country = document.getElementById("country").value;
+  console.log(country);
+
+  let customerList = []; //create a list of songs searched for
+  
+  for(let i=0; i<customers.length;i++){
+    let customer = customers[i] //get each sog
+    //make sure the list is no
+    if( customer.Country == country ) {
+          //add to the new list
+          customerList.push(customer);
+    }
+  }
+  console.log(`number found ${customerList.length}`)
+  generateCards(customerList);  
+}
+
+
 
